c46a97787c3d8133a8d52594da6c124ab2133f69
Abdjb <63862086+Abdjb@users.noreply.github.com>
2022-10-18 14:51:38 +0800

    minor css update for mobile

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -19,7 +19,7 @@
                 </a>
             </div>
             {{-- CONTAINER FOR INPUT --}}
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-11/12 sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 <form>
                     {{-- Username --}}
                     <div>
