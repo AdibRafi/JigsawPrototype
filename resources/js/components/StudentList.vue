73d57d0dbd31c182c115c7ecc1f9f8261b089f15
Abdjb <63862086+Abdjb@users.noreply.github.com>
2022-11-18 21:57:55 +0800

    change img path data

--- resources/js/components/StudentList.vue
+++ resources/js/components/StudentList.vue
@@ -1,7 +1,7 @@
 <template>
    <div v-for="data in datas">
       <div class="flex items-center gap-4 p-2">
-         <img src="images/emoji.png" alt="studentPhoto" class=" w-14 h-14 rounded-full object-scale-down">
+         <img :src="data.img" alt="studentPhoto" class=" w-14 h-14 rounded-full object-scale-down">
          <div class="flex flex-col">
             <strong>{{ data.name }}</strong>
             <span>{{ data.group }}</span>
@@ -13,12 +13,12 @@
 export default {
    setup() {
       const datas = [
-         { name: 'student one', group: 'group one' },
-         { name: 'student two', group: 'group two' },
-         { name: 'student three', group: 'group three' },
-         { name: 'student four', group: 'group four' },
-         { name: 'student five', group: 'group five' },
-         { name: 'student six', group: 'group six' },
+         { name: 'student one', group: 'group one', img: '/images/emoji.png' },
+         { name: 'student two', group: 'group two', img: '/images/emoji.png' },
+         { name: 'student three', group: 'group three', img: '/images/emoji.png' },
+         { name: 'student four', group: 'group four', img: '/images/emoji.png' },
+         { name: 'student five', group: 'group five', img: '/images/emoji.png' },
+         { name: 'student six', group: 'group six', img: '/images/emoji.png' },
       ]
 
       return { datas }
