eeb6e9b6c45434a1f93acf1603c3c362c56fbe50
Abdjb <63862086+Abdjb@users.noreply.github.com>
2023-01-06 12:50:48 +0800

    change into daisy

--- resources/js/components/lecturer/CreateTopic.vue
+++ resources/js/components/lecturer/CreateTopic.vue
@@ -4,27 +4,34 @@
             <div class="flex flex-col items-center pt-6">
                 <h1 class=" text-xl"> Create Topic</h1>
                 <p> contoh -> {{ name }} | {{ description }}</p>
-                <div class="w-11/12 sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                <div class="w-7/12 sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                     <form @submit="formSubmit">
                         <div>
                             <InputForm input-id="topic" input-type="text"
-                                       label-for="topic" label-name="Topic" v-model="name"/>
+                                       label-for="topic" label-name="Topic Name" v-model="name"/>
                         </div>
                         <div class="mt-4">
-                            <InputForm input-id="module" input-type="text"
-                                       label-for="module" label-name="Module" v-model="description"/>
+                            <span class="block font-medium text-gray-700 text-sm">Number of Modules</span>
+                            <input type="range" min="0" max="100" value="25" class="mt-2 range range-primary w-80" step="25"/>
+                            <div class="w-full flex justify-between text-xs px-2 w-80">
+                                <span>2</span>
+                                <span>3</span>
+                                <span>4</span>
+                                <span>5</span>
+                                <span>6</span>
+                            </div>
                         </div>
                         <div class="mt-4">
                             <InputForm input-id="" input-type="text"
-                                       label-for="" label-name="Expert Session" />
+                                       label-for="" label-name="Maximum Time for Expert Session" />
                         </div>
                         <div class="mt-4">
                             <InputForm input-id="" input-type="text"
-                                       label-for="" label-name="Jigsaw Session" />
+                                       label-for="" label-name="Maximum Time for Jigsaw Session" />
                         </div>
 
-                        <div class="flex items-center justify-end mt-4">
-                            <SubmitButton button-name="Create Group" button-method="clickTest"/>
+                        <div class="mt-6">
+                            <SubmitButton button-name="Proceed" button-method="clickTest"/>
                         </div>
                     </form>
                 </div>
