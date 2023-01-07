835ee713eec2806cf4a5569e9c9a00225ff590f4
Abdjb <63862086+Abdjb@users.noreply.github.com>
2023-01-07 17:57:35 +0800

    change layout into daisyUI

--- resources/js/components/lecturer/Dashboard.vue
+++ resources/js/components/lecturer/Dashboard.vue
@@ -1,36 +1,42 @@
 <template>
-    <div class="dashboard">
-        <div class="bg-blue-300 p-10 rounded-lg">
-            <div class=" flex flex-row flex-wrap">
+    <div class="font-sans text-gray-900 antialiased">
+        <div class="pt-6 bg-gray-100 justify-center rounded-xl min-h-screen">
+            <div class="flex flex-wrap items-center p-6">
                 <div v-for="data in datas" key="data">
-                    <div class="m-8 p-9 bg-slate-400 text-2xl rounded-2xl">
-                        <h1>{{ data.name }}</h1>
-                        <h1>{{ data.code }}</h1>
+                <div class="card w-96 bg-green-700 text-primary-content m-4">
+                    <div class="card-body">
+                                <h2 class="card-title">{{ data.name }}</h2>
+                                <p>{{ data.code }}</p>
+                                <div class="card-actions justify-end">
+                                    <button class="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 </template>
 
 <script setup>
 const datas = [
-   {
-      name: 'Class1',
-      code: 'TGD1202'
-   },
-   {
-      name: 'Class2',
-      code: 'TCP1302'
-   },
-   {
-      name: 'Class3',
-      code: 'TMU2231'
-   },
-   {
-      name: 'Class4',
-      code: 'TSP3301'
-   }
+    {
+        name: 'Class1',
+        code: 'TGD1202'
+    },
+    {
+        name: 'Class2',
+        code: 'TCP1302'
+    },
+    {
+        name: 'Class3',
+        code: 'TMU2231'
+    },
+    {
+        name: 'Class4',
+        code: 'TSP3301'
+    }
 
 ]
 </script>
